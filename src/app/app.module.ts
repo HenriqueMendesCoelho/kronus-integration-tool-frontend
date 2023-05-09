fdfd89cce96df22334c83f128c854668596c033d
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-05-09 19:19:15 -0300

    refactor: Login page created base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 import { LayoutWithNavbarComponent } from './layout/layout-with-navbar/layout-with-navbar.component';
 import { LayoutWithoutNavbarComponent } from './layout/layout-without-navbar/layout-without-navbar.component';
@@ -17,6 +19,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeToolbarComponent } from './components/home/home-toolbar/home-toolbar.component';
 import { HomeTitleComponent } from './components/home/home-title/home-title.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +30,7 @@ import { HomeTitleComponent } from './components/home/home-title/home-title.comp
     LoginComponent,
     HomeToolbarComponent,
     HomeTitleComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,6 +41,8 @@ import { HomeTitleComponent } from './components/home/home-title/home-title.comp
     MatButtonModule,
     MatTableModule,
     MatSnackBarModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
+  email: string = '';
+  password: string = '';
+  hide: boolean = true;
 }
