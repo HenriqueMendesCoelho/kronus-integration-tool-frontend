a6efdc407b20cd10cc8bb50ab5a661de49e7701e
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-05-10 03:32:24 -0300

    refactor: layout-with-navbar create component for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LayoutWithNavbarComponent } from './layout/layout-with-navbar/layout-with-navbar.component';
 import { LayoutWithoutNavbarComponent } from './layout/layout-without-navbar/layout-without-navbar.component';
@@ -20,6 +20,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeToolbarComponent } from './components/home/home-toolbar/home-toolbar.component';
 import { HomeTitleComponent } from './components/home/home-title/home-title.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     HomeToolbarComponent,
     HomeTitleComponent,
     NotFoundComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,6 +45,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     MatSnackBarModule,
     MatInputModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
