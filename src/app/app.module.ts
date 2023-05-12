3388244a39073b7a2d7f6f1052830dda98d56534
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-05-12 13:33:33 -0300

    feat: open dialog on toolbar and refresh table in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,6 +13,8 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { LayoutWithNavbarComponent } from './layout/layout-with-navbar/layout-with-navbar.component';
 import { LayoutWithoutNavbarComponent } from './layout/layout-without-navbar/layout-without-navbar.component';
@@ -21,6 +24,7 @@ import { HomeToolbarComponent } from './components/home/home-toolbar/home-toolba
 import { HomeTitleComponent } from './components/home/home-title/home-title.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DialogCreateApikeyComponent } from './components/home/home-toolbar/dialog-create-apikey/dialog-create-apikey.component';
 
 @NgModule({
   declarations: [
@@ -33,10 +37,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomeTitleComponent,
     NotFoundComponent,
     NavbarComponent,
+    DialogCreateApikeyComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
@@ -46,6 +52,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
+    MatTooltipModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
